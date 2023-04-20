33bd849138bf0fe828750a484fc45533437e890c
seonwooke <seonookkim@gmail.com>
2023-04-20 23:00:19 +0900

    :art: Server connection with Client
    Server connection with Client

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const Products = () => {
+const Products = ({ name, imagePath }) => {
     return (
-        <div>Products</div>
+        <div style={{ textAlign: "center" }}>
+            <img
+                style={{ width: "75%" }}
+                src={`http://localhost:4000/${imagePath}`}
+                alt={`${name} product`}
+            />
+            <form style={{ marginTop: "10px" }}>
+                <labe style={{ textAlign: "right" }}>{name}</labe>
+                <input
+                    style={{ marginLeft: "7px" }}
+                    type="number"
+                    name="quantity"
+                    min="0"
+                    defaultValue={0}
+                />
+            </form>
+        </div>
     )
 }
 
